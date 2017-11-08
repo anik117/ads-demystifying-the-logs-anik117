1510e967f398492f42fe24a4693ea1f7d6548a12
Tanvir Ahassan Anik <anik880@gmail.com>
2017-11-08 06:48:37 +0600

    assignment 6

---
+++ assignment6/reducer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+
+hit2009 = 0
+hit2010 = 0
+hit2011 = 0
+
+
+for line in sys.stdin:
+  if '2009' in line.strip()
+  	hit2009 = hit2009 + 1
+  if '2010' in line.strip()
+  	hit2010 = hit2010 + 1
+  if '2011' in line.strip()
+  	hit2011 = hit2011 + 1
+print (hit2009)
+print (hit2010)
+print (hit2011)
